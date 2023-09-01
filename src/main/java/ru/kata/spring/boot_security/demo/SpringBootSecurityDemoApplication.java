afe205f8f84e7385c3cb9917cc16b70925bc4a5a
HP OMEN <nord2002d@yandex.ru>
2023-09-01 16:31:59 +0300

    initial commit

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -1,14 +1,7 @@
 package ru.kata.spring.boot_security.demo;
-import org.hibernate.SessionFactory;
-import org.hibernate.procedure.ProcedureOutputs;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.ParameterMode;
-import javax.persistence.StoredProcedureQuery;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
