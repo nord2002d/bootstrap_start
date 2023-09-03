496be3fdac71d27c03bcf4c1df8155d8fa7edce1
HP OMEN <nord2002d@yandex.ru>
2023-09-03 23:55:22 +0300

    Изменил SQL script, Удалили Util класс.

--- src/main/resources/ScriptSQL/procedure.sql
+++ src/main/resources/ScriptSQL/procedure.sql
@@ -1,3 +1,5 @@
+#    Можно выбрать создание или обновление таблицы
+#   Создание таблицы
 
 CREATE TABLE IF NOT EXISTS  users(
                                      id INT NOT NULL AUTO_INCREMENT,
@@ -17,3 +19,11 @@ CREATE TABLE IF NOT EXISTS user_role (
                                          roles  varchar(255) NOT NULL );
 INSERT INTO user_role VALUES (1, 'ADMIN');
 INSERT INTO user_role VALUES (2, 'USER');
+
+# Обновление таблицы
+
+# ALTER TABLE  users
+# ADD email varchar(255) NOT NULL;
+#
+# REPLACE users VALUES (1,'admin','Sokolov', 11,'$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq','admin@a.ru');
+# REPLACE users VALUES (2, 'user','Petrov', 12,'$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC','user@u.ru');
