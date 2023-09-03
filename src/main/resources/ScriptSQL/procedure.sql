45604637c62d6b563b6c0201ccc98c9f68aebca0
HP OMEN <nord2002d@yandex.ru>
2023-09-03 21:00:12 +0300

    Изменил SQL script, Удалили Util класс.

--- src/main/resources/ScriptSQL/procedure.sql
+++ src/main/resources/ScriptSQL/procedure.sql
@@ -1,12 +1,19 @@
-#       Вставка в новосозданныую таблицу
 
-INSERT INTO users VALUES (1, 11,'admin@a.ru', 'admin', '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq', 'Sokolov');
-INSERT INTO users VALUES (2, 12, 'user@u.ru', 'user', '$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC', 'Petrov');
+CREATE TABLE IF NOT EXISTS  users(
+                                     id INT NOT NULL AUTO_INCREMENT,
+                                     name varchar(30) NOT NULL,
+                                     sur_name varchar(30) NOT NULL,
+                                     age INT NOT NULL,
+                                     password varchar(255) NOT NULL,
+                                     email varchar(255) NOT NULL,
+                                     PRIMARY KEY(id));
+
+INSERT INTO users VALUES (1,'admin','Sokolov', 11,'$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq','admin@a.ru');
+INSERT INTO users VALUES (2, 'user','Petrov', 12,'$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC','user@u.ru');
+
+
+CREATE TABLE IF NOT EXISTS user_role (
+                                         user_id INT NOT NULL,
+                                         roles  varchar(255) NOT NULL );
 INSERT INTO user_role VALUES (1, 'ADMIN');
 INSERT INTO user_role VALUES (2, 'USER');
-
-
-#       Вставка в таблицу созданную в задании 3.1.3
-
-# REPLACE INTO users VALUES (1, 11,'admin', '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq', 'Sokolov','admin@a.ru');
-# REPLACE INTO users VALUES (2, 12, 'user', '$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC', 'Petrov','user@u.ru');
